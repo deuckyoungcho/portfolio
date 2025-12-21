fb95b99bbba89fb77748e1ab338d57c919893a9e
deuckyoungcho <cps3upp@gmail.com>
2025-12-21 23:21:54 +0900

    fix: change vite setting

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/deuckyoungcho/portfolio/",
+  base: "/portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
